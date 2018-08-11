403f68e874fe0d17371417eca72a54e595ebaabd
Shannon Holmes <snholmes13@gmail.com>
2018-08-11 17:42:54 +0100

    continue to integrate sorting hat

--- hogwarts_controller.rb
+++ hogwarts_controller.rb
@@ -2,7 +2,7 @@ require('sinatra')
 require('sinatra/contrib/all')
 require('pry')
 
-require_relative('./models/SH-sort.rb')
+# require_relative('./models/SHsort.rb')
 require_relative('./models/student.rb')
 require_relative('./models/house.rb')
 also_reload('./models/*')
@@ -16,23 +16,27 @@ get '/students/new' do
   erb (:SHhome)
 end
 
-# get '/SH' do
-#   erb(:SHhome)
-# end
-
-get '/SH/sort' do
-  erb(:SHsort)
+post '/students/result' do
+  @student = Student.new(params)
+  # house_array = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+  # sort = House.new(house_name = house_array.sample())
+  # @house_name = sort.house_sort()
+  # @student_name = "#{@first_name} #{@second_name}"
+  erb(:result)
 end
 
-# get '/result' do
-#   erb(:result)
-# end
-
-post '/SH/result' do
+get '/students/result' do
+  @student_name = "#{@first_name} #{@second_name}"
   house_array = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
-  sort = Sort.new(house_name = house_array.sample())
-  @sort = sort.house_sort()
-  erb(:SHresult)
+  sort = House.new(house_name = house_array.sample())
+  @house_name = sort.house_sort()
+  return house_name
+  @student.save()
+  erb(:result)
+end
+
+get '/students/sort' do
+  erb(:new)
 end
 
 # INDEX
@@ -40,12 +44,6 @@ get '/students' do
   @students = Student.all()
   erb (:index)
 end
-
-# NEW
-# get '/students/new' do
-#   @houses = House.all()
-#   erb (:new)
-# end
 
 # SORT
 get '/students/sort' do
@@ -66,3 +64,31 @@ end
 get '/images/:file' do
   send_file File.expand_path(params[:file], settings.public_folder)
 end
+
+
+
+# NEW
+# get '/students/new' do
+#   erb (:new)
+# end
+
+# CREATE
+# post '/students' do
+#   @student = Student.new(params)
+#   @student.save()
+#   erb (:create)
+# end
+
+# get '/SH' do
+#   erb(:SHhome)
+# end
+
+# NEW
+# get '/students/new' do
+#   @houses = House.all()
+#   erb (:new)
+# end
+
+# get '/result' do
+#   erb(:result)
+# end
--- models/house.rb
+++ models/house.rb
@@ -7,15 +7,31 @@ class House
   def initialize(options)
     @id = options['id'].to_i() if options['id']
     @name = options['name']
-    @logo = options['logo']
+    # @logo = options['logo']
+  end
+
+  # def student_name()
+  #   return "#{@first_name} #{@second_name}"
+  # end
+
+  def house_sort()
+    if @name == "Gryffindor"
+      return "Your motivation in life is found in trusting your moral intuitions and going with your gut... #{students.first_name}#{students.last_name}, you belong in Gryffindor!!"
+    elsif @name == "Hufflepuff"
+      return "#{students.first_name}#{students.last_name}, you work hard, are patient, and value justice and loyalty... Hufflepuff is where #{students.first_name}#{students.last_name} belongs!"
+    elsif @name == "Ravenclaw"
+      return "You are intelligent, creativite, eager to learn, and witty. Ravenclaw is where you belong, #{students.first_name}#{students.last_name}!"
+    else @name == "Slytherin"
+      return "You are ambitious, cunning and resourceful... You will do anything to get your way! #{students.first_name}#{students.last_name}, Slytherin will help you on your way to greatness!"
+    end
   end
 
   def save()
     sql ="INSERT INTO houses (name)
     VALUES ($1) RETURNING *"
     values = [@name]
-    student_data = SqlRunner.run(sql, values)
-    @id = student_data.first()['id'].to_i()
+    house_data = SqlRunner.run(sql, values)
+    @id = house_data.first()['id'].to_i()
   end
 
   def update()
@@ -48,19 +64,6 @@ class House
     house = SqlRunner.run(sql, values)
     result = House.new(house.first())
     return result
-  end
-
-  def house_sort()
-
-    if @name == "Gryffindor"
-      return "Your motivation in life is found in trusting your moral intuitions and going with your gut... #{student_name}, you belong in Gryffindor!!"
-    elsif @name == "Hufflepuff"
-      return "#{student_name}, you work hard, are patient, and value justice and loyalty... Hufflepuff is where #{student_name} belongs!"
-    elsif @name == "Ravenclaw"
-      return "You are intelligent, creativite, eager to learn, and witty. Ravenclaw is where you belong, #{student_name!}"
-    else @name == "Slytherin"
-      return "You are ambitious, cunning and resourceful... You will do anything to get your way! #{student_name}, Slytherin will help you on your way to greatness!"
-    end
   end
 
 end
